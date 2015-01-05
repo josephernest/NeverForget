8dffecb8738221334a296fe823f07c6cf7601aca
josephernest <nouvellecollection@gmail.com>
2015-01-05 23:30:44 +0100

    Bugfix: UTF8 saving to file

--- src/neverforget.cpp
+++ src/neverforget.cpp
@@ -11,6 +11,7 @@
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+HWND hwndMain = NULL;
 HWND hwndEdit = NULL;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
@@ -26,12 +27,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
     HWND taskBar = FindWindow(L"Shell_traywnd", NULL);
     GetWindowRect(taskBar, &rect);
 
-    HWND hwnd = CreateWindowEx(WS_EX_TOOLWINDOW | WS_EX_LAYERED, wc.lpszClassName, 0, WS_POPUP | WS_VISIBLE | WS_SYSMENU, 0, 3 * rect.top / 4, 300, rect.top / 4, 0, 0, 0, 0);
-    hwndEdit = CreateWindowEx(0, L"EDIT", NULL, WS_CHILD | WS_VISIBLE | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL, 0, 0, 300, rect.top / 4, hwnd, 0, (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), NULL);      
-    SetLayeredWindowAttributes(hwnd, 0, 192, LWA_ALPHA);
-    ShowWindow(hwnd, nCmdShow);
+    hwndMain = CreateWindowEx(WS_EX_TOOLWINDOW | WS_EX_LAYERED, wc.lpszClassName, 0, WS_POPUP | WS_VISIBLE | WS_SYSMENU, 0, 3 * rect.top / 4, 300, rect.top / 4, 0, 0, 0, 0);
+    hwndEdit = CreateWindowEx(0, L"EDIT", NULL, WS_CHILD | WS_VISIBLE | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL, 0, 0, 300, rect.top / 4, hwndMain, 0, (HINSTANCE) GetWindowLong(hwndMain, GWL_HINSTANCE), NULL);      
+    SetLayeredWindowAttributes(hwndMain, 0, 192, LWA_ALPHA);
+    ShowWindow(hwndMain, nCmdShow);
 
-    std::ifstream t("neverforget.txt");
+    std::ifstream t("neverforget.txt", std::ios::in  | std::ios::binary);
     std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 
     SendMessageA(hwndEdit, WM_SETTEXT, 0, (LPARAM) str.c_str()); 
@@ -50,19 +51,19 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
-    case WM_DESTROY:
+        case WM_DESTROY:
         {
             int len = GetWindowTextLengthA(hwndEdit);
             std::vector<char> text(len+1);
-            GetWindowTextA(hwndEdit, &text[0], len);
-            std::ofstream file("neverforget.txt");
+            GetWindowTextA(hwndEdit, &text[0], len+1);
+            std::ofstream file("neverforget.txt", std::ios::out  | std::ios::binary);
             file.write(&text[0], len);
             file.close();
             PostQuitMessage(0);
             return 0;
         } 
 
-    case WM_PAINT:
+        case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
@@ -72,6 +73,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             return 0;            
         }
 
+        case WM_WINDOWPOSCHANGING:
+        {
+            ShowWindow(hwndMain, SW_SHOW);
+            return 0;  
+        }
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
